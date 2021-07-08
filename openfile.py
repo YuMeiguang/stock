234195d705fe39f40d166c88fc2059fdf5313058
yumeiguang <470215591@qq.com>
2021-07-08 21:35:04 +0800

    asd

--- openfile.py
+++
@@ -1,9 +0,0 @@
-try:
-  fh = open("testfile","w")
-  fh.write("this is a file")
-except IOError:
-  print("this is a error ")
-
-else:
-  print("write file success")
-  fh.close()
--- test-redis.py
+++
@@ -1,6 +0,0 @@
-import redis
-import constant
-r = redis.Redis(host=constant.redisHost,port=constant.redisPort,password=constant.redisPassword,db=0,decode_responses=True)
-r.set('foo','for')
-r.get('foo')
-print(r.get('foo'))
